5e88a581a55c9524d6038a12980840ce2bfbf28b
Felipedmgs <felipedd@hotmail.com>
2025-02-18 19:11:11 -0300

    Operadores

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,32 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        
+        String nome = "Felipe", rs;
+        
+        int a, b, c;
+        a=1;
+        b=1;
+        c=1;
+
         System.out.println("Hello, World!");
+        System.out.println(a + b + nome + c);
+        System.out.println(++a);
+
+        //ternario
+        rs = a == b ? "true" : "false";
+        System.out.println(rs);
+
+        ///operadores
+        boolean sn = a == b;
+        sn = a > b;
+        //sn = a < b;
+        System.out.println("Numeros são iguais? " + sn);
+
+        System.out.println("Var são iguais? " + nome.equals(rs));
+
+
+
+
     }
 }
+
