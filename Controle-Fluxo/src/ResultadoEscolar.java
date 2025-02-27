b5532f36c5b86686d81f37d6ca4d3ad9330fd9a8
Felipedmgs <felipedd@hotmail.com>
2025-02-26 22:07:37 -0300

    sistema_agencia

--- Controle-Fluxo/src/ResultadoEscolar.java
+++ Controle-Fluxo/src/ResultadoEscolar.java
@@ -1,7 +1,9 @@
 public class ResultadoEscolar {
 
     public static void main(String[] args) {
-        int nota =4;
+        int nota =7;
+
+        System.out.println("Nota a ser calculada: " + nota);
 
         if(nota >= 7) {
             System.out.println("Aprovado");
@@ -11,6 +13,35 @@ public class ResultadoEscolar {
         else {
             System.out.println("Reprovado");
         }
+
+
+        int notam = 9;
+        String rs;
+        System.out.println("Nota de Matemática: " + notam);
+
+        rs = notam >= 7 ? "Aprovado" : notam >= 5 ? "Recuperação" : "Reprovado";
+        System.out.println(rs);
+
+        int notaf = 5;
+        System.out.println("Nota de Física " + notaf);
+
+        switch (notaf) {
+            case 7:
+                System.out.println("Aprovado");                
+                break;
+
+            case 5:
+                System.out.println("Recuperação");
+                break;
+
+                case 0:
+                System.out.println("Recuperação");
+        
+            default:
+                   System.out.println("Recuperação");
+                break;
+        }
     }
     
 }
+
